a6bf71396d48f23227f16129c9d4f290137fba33
Zvertiolli <zvertiolli@gmail.com>
2020-12-22 23:52:46 +0300

    commit 2

--- src/gitTutorial.java
+++ src/gitTutorial.java
@@ -2,5 +2,6 @@ public class gitTutorial {
     public static void main(String[] args) {
         System.out.println("Hello GIT!");
         System.out.println("Hello Hello!");
+        System.out.println("commit");
     }
 }
